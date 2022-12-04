30d399bf82386bfee37a04487dd84ab4883bff25
Ahmed-Trabelsi0 <trabelsig37@gmail.com>
2022-12-04 12:50:59 +0100

    revert commit

--- src/main.js
+++
@@ -1,5 +0,0 @@
-import { createApp } from 'vue';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../node_modules/bootstrap-icons/font/bootstrap-icons.css';
-import App from './App.vue';
-createApp(App).mount('#app');
