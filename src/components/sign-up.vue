ebc77a9742deac3bcdca39eeded1d89d95368d94
Ahmed-Trabelsi0 <trabelsig37@gmail.com>
2022-12-10 12:48:10 +0100

    add ratings

--- src/components/sign-up.vue
+++ src/components/sign-up.vue
@@ -1,37 +1,21 @@
 <template>
-  <div class="sign-up container w-100">
+  <div class="sign-up container d-flex justify-content-between">
     <div class="child container p-5 d-flex flex-column justify-content-center">
       <form>
-        <label for="name">Name</label>
+        <label for="name">Name:</label>
         <input type="text" name="name" v-model="name" required />
-        <label for="password">Password</label>
-        <input type="password" v-model="password" required />
-        <select v-model="selected">
-          <option value="Developer">Developer</option>
-          <option value="Dentiste">Dentiste</option>
-          <option value="Plasticien">Plasticien</option>
-          <option value="Roler">Roler</option>
-        </select>
-        <br>
-        <label for="terms">Terms and Conditions</label>
-        <input type="checkbox" name="terms" v-model="terms" required />
-        <label for="names">Ahmed</label>
-        <input type="checkbox" name="names" value="ahmed" v-model="names"/>
-        <label for="names">Emna</label>
-        <input type="checkbox" name="names" value="emna" v-model="names"/>
-        <label for="names">Ghada</label>
-        <input type="checkbox" name="names" value="ghada" v-model="names"/>
-        <button type="submit">submit</button>
+        <label for="Rating">Rating:</label>
+        <textarea type="" name="Rating" v-model="rating" required></textarea>
+        <button class="btn btn-primary d-flex m-auto" @click.prevent="addRating">Send<i class="bi bi-send"></i></button>
       </form>
     </div>
-    <div
-      class="child container p-5 d-flex flex-column justify-content-center text-center"
-    >
-      <h1>Name: {{ name }}</h1>
-      <h1>Password: {{ password }}</h1>
-      <h1>{{ selected }}</h1>
-      <h1>{{ terms }}</h1>
-      <h1 v-for="(name, index) in names" :key="index">{{name}}</h1>
+    <div class="ratings d-flex flex-column overflow-auto">
+      <h3 class="display-5 ms-5">Ratings</h3>
+      <div v-for="(user, index) in users" :key="index" class="card bg-light p-3 m-3 ">
+        <h3>{{ user.name }}</h3>
+        <hr />
+        <p>{{ user.rating }}</p>
+      </div>
 
     </div>
   </div>
@@ -42,11 +26,28 @@ export default {
   data() {
     return {
       name: "",
-      password: "",
-      selected: "Developer",
-      terms: "false",
-      names: [],
+      rating: "",
+      users: [
+        {
+          name: "Ahmed",
+          rating:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat iste omnis minima eligendi exercitationem reprehenderit et nam molestias dolorum corporis asperiores, ex similique repellat praesentium quis inventore debitis quasi repellendus.",
+        },
+      ],
     };
+  },
+  methods: {
+    addRating() {
+      if (this.name && this.rating) {
+        this.users.push({ name: this.name, rating: this.rating });
+        this.name = "";
+        this.rating = "";
+      }
+    },
+    remove(e) {
+      console.log(e);
+      e.target.remove();
+    },
   },
 };
 </script>
@@ -55,7 +56,7 @@ export default {
 .signUp {
   background-color: #eee;
 }
-.child {
-  background-color: white;
+.ratings {
+  max-height: 45vh;
 }
 </style>
